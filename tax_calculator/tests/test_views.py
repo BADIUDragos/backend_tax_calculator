2459b5bb922485f15fdb393e5cdf23f4006cad9c
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-04-22 23:23:14 -0400

    test views comments

---
+++ tax_calculator/tests/test_views.py
@@ -0,0 +1,14 @@
+
+def test_serializer_data_invalid:
+    #not int, not float, not supported year etc, just like in the previous submission
+    pass
+
+def test_serializer_data_valid():
+    pass
+
+def test_good_mocked_response_from_get_tax_brackets():
+    pass
+
+def test_bad_mocked_response_from_get_tax_brackets():
+    pass
+
